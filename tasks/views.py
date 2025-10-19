de3f245df90c3fd4aa52ab620e614d8e6830c40d
David Baiden Amissah <davidbamissah@gmail.com>
2025-10-19 23:31:23 +0000

    Create and Add views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
