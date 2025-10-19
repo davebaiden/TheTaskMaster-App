5226363a0999a1fa8aea4677ac47be7e8c4e745d
David Baiden Amissah <davidbamissah@gmail.com>
2025-10-19 21:05:22 +0000

    Created fields to models.py in tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium')
+        ('high', 'High'),
+    ]
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending')
+        ('completed', 'Completed'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name = 'tasks')
+    title = models.CharField(max_length = 255)
+    description = models.TextField(blank=True)
+    due_date = models.DateField()
+    priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES_CHOICES, default='Medium')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='Pending')
+    completed_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
