6e053ab57651d79fb5d8fd2b7d76725cee56bd62
David Baiden Amissah <davidbamissah@gmail.com>
2025-10-19 23:04:50 +0000

    Edited models.py, and run migrations and migrated

--- tasks/models.py
+++ tasks/models.py
@@ -4,20 +4,20 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     PRIORITY_CHOICES = [
         ('low', 'Low'),
-        ('medium', 'Medium')
+        ('medium', 'Medium'),
         ('high', 'High'),
     ]
 
     STATUS_CHOICES = [
-        ('pending', 'Pending')
+        ('pending', 'Pending'),
         ('completed', 'Completed'),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name = 'tasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='tasks')
     title = models.CharField(max_length = 255)
     description = models.TextField(blank=True)
     due_date = models.DateField()
-    priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES_CHOICES, default='Medium')
+    priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES, default='Medium')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='Pending')
     completed_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
