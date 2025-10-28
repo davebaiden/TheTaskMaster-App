3097f5ea26b3444d93b421d9b9e06ed6f07f73fd
David Amissah <david.amissah@margins-id.com>
2025-10-28 16:02:04 +0000

    add url for users

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from .views import TaskViewSet, UserTaskViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'user-tasks', UserTaskViewSet, basename='user-tasks')
 
 urlpatterns = [
     path('', include(router.urls)),
